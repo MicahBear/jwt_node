cb10b6198e3f1b60ffd4bf256cef3b0e494873e8
MicahBear <micah.b.elm@gmail.com>
2023-03-28 15:21:48 -0700

    update server.js file with task routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
 const mainRoutes = require('./routes/main');
-const userAuth = require('./routes/userAuth')
+const taskRoutes = require('./routes/task')
+// const userAuth = require('./routes/userAuth')
 const PORT = process.env.PORT || 8000
 const app = express()
 
+app.use('/task', taskRoutes)
 app.use('/', mainRoutes)
 // app.use('/userAuth', userAuth)
 
