0459e272fec9b8b36c2179a7649d6b29bc0a15cd
MicahBear <micah.b.elm@gmail.com>
2023-03-28 16:26:09 -0700

    update: server.js file install cors, create errorhandler from middleware

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
-const express = require('express');
+const express = require('express')
 const dotenv = require('dotenv').config()
 const logger = require('morgan')
-const mainRoutes = require('./routes/main');
+const { errorHandler } = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const mainRoutes = require('./routes/main')
 const taskRoutes = require('./routes/task')
 // const userAuth = require('./routes/userAuth')
 const PORT = process.env.PORT || 8000
 const app = express()
 
-app.use(express.json())
-//Logging
+// Logging
 app.use(logger("dev"));
+app.use(cors())
 
+// Parsing
+app.use(express.json())
+app.use(cookieParser())
+
+// Routes
 app.use('/', mainRoutes)
 app.use('/task', taskRoutes)
 // app.use('/userAuth', userAuth)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT} `)
