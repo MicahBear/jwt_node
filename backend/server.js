b09e75c7d17637d0d43b890cae8656d994b48aa0
MicahBear <micah.b.elm@gmail.com>
2023-03-28 15:57:56 -0700

    update: use parse json to send or receive. use logger during dev development

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
+const logger = require('morgan')
 const mainRoutes = require('./routes/main');
 const taskRoutes = require('./routes/task')
 // const userAuth = require('./routes/userAuth')
 const PORT = process.env.PORT || 8000
 const app = express()
+
+app.use(express.json())
+//Logging
+app.use(logger("dev"));
 
 app.use('/', mainRoutes)
 app.use('/task', taskRoutes)
