361c9d2034d68e80ee357a275e9abc43b04bcc31
MicahBear <micah.b.elm@gmail.com>
2023-03-29 10:50:55 -0700

    created task model, update models folder name, updated task controller to intake model.

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,11 +1,22 @@
 const asyncHandler = require('express-async-handler')
+const Task = require('../models/taskModel')
 
 module.exports = {
     getTasks: asyncHandler(async (req, res) => {
-        res.send('grab tasks')
+        const tasks = await Task.find()
+
+        res.status(200).json(tasks)
     }),
     postTask: asyncHandler(async (req, res) => {
-        res.send('create task')
+        if (!req.body.text) {
+            res.status(400)
+        }
+
+        const task = await Task.create({
+            text: req.body.text
+        })
+
+        res.status(200).json(task)
     }),
     putTask: asyncHandler(async (req, res) => {
         res.send('update task')
---
+++ backend/models/taskModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+
+const taskSchema = mongoose.Schema({
+    text: {
+        type: String,
+        required: [true, 'Please add title']
+    }
+},
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('Task', taskSchema)
