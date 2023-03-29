c29d3d4703ec1c40506d46b7e9f64f33915a974d
MicahBear <micah.b.elm@gmail.com>
2023-03-29 14:57:54 -0700

    update: route name

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const taskController = require('../controllers/taskController')
 
 //!needs authcontroller
-router.get('/:id', taskController.getTasks);
+router.get('/', taskController.getTasks);
 router.post('/create/:id', taskController.postTask);
 router.put('/update/:id', taskController.putTask);
 router.delete('/delete/:id', taskController.deleteTask);
