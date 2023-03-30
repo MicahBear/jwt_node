5308645810ff0420f7a581a6ed43cc5b0e3f19d3
MicahBear <micah.b.elm@gmail.com>
2023-03-30 16:19:29 -0700

    updated: task.js : removed id requirement on route for get tasks temp for now. add back to be a protected path once auth is in

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController')
 
-//!needs authcontroller
+//!needs auth
 router.get('/', taskController.getTasks);
-router.post('/create/:id', taskController.postTask);
+router.post('/create/', taskController.postTask);
 router.put('/update/:id', taskController.putTask);
 router.delete('/delete/:id', taskController.deleteTask);
 
