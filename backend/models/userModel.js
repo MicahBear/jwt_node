f477e97ebf1b8a7ec75991bfa834c84c9b8bbf0c
MicahBear <micah.b.elm@gmail.com>
2023-04-04 12:56:31 -0700

    update user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose')
 
-const { Schema } = mongoose
-
-const userSchema = new Schema({
-
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: [{
+        type: String,
+        default: "Employee"
+    }],
+    active: {
+        type: Boolean,
+        default: true
+    }
 })
+
+module.exports = mongoose.model('User', userSchema)
