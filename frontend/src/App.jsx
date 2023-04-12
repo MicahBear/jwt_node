553da752a29efd605fb62aad71ea134c822456ce
MicahBear <micah.b.elm@gmail.com>
2023-04-12 06:19:47 -0700

    creating routing for login and register on app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
 
 
   return (
-    <div >
-      <h1>Frontend</h1>
+    <>
+    <Router>
+    <div className='container'>
+      <Routes>
+        <Route path='/login' element={Login}/>
+        <Route path='/Register' element={Register}/>
+      </Routes>
     </div>
+    </Router>
+    </>
   )
 }
 
