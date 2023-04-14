e1caaee5c828e4e4e2dd7cafce3dadb588d7c1bc
MicahBear <micah.b.elm@gmail.com>
2023-04-14 10:39:51 -0700

    update main with react router browswer and routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BroswerRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BroswerRouter>
+     <Routes>
+      <Route path='/*' element={<App/>}/>
+     </Routes>
+    </BroswerRouter>
   </React.StrictMode>,
 )
