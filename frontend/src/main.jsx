69c13ea5da999ab4087a1441ff884e443a991aa0
MicahBear <micah.b.elm@gmail.com>
2023-04-14 10:42:45 -0700

    update with comment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BroswerRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
-
+//the path /* allows us to have nested routes.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BroswerRouter>
