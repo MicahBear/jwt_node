93d561e41a82232322f2b392a2ac9bb54c9dd25d
MicahBear <micah.b.elm@gmail.com>
2023-04-17 14:53:48 -0700

    update import of employee dashboardpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
-import Login from './pages/Login'
+import Login from './features/auth/Login'
+import EmpDashPage from './pages/EmpDashPage'
 import Public from './pages/Public'
 import Register from './pages/Register'
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<Layout/>}> 
          <Route index element={<Public/>}/> 
          <Route path='login' element={<Login/>}/>
+         <Route path='dash' element={<EmpDashPage/>}/>
          <Route path='/Register' element={<Register/>}/>
         </Route>
       </Routes>
