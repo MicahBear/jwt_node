79d656985b6bd068bb62561ddeca3d35f0bb90c8
MicahBear <micah.b.elm@gmail.com>
2023-04-21 21:12:46 -0700

    update: seperate public route and protected route .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
-import Layout from './components/Layout'
+import Layout from './layouts/Layout'
+// import EmpLayout from './components/EmpLayout'
 import Login from './features/auth/Login'
 import Welcome from './features/auth/Welcome'
-import TasksList from './features/tasks/TasksList'
+import NotesPage from './features/notes/NotesPage'
+import TasksPage from './features/tasks/TasksPage'
 import UsersList from './features/users/UsersList'
-import EmpDashPage from './pages/EmpDashPage'
+// import EmpDashPage from './pages/EmpDashPage'
+import EmpLayout from './layouts/EmpLayout'
 import Public from './pages/Public'
 import Register from './pages/Register'
 
@@ -19,22 +22,25 @@ function App() {
          {/** public routes */}
          <Route index element={<Public/>}/> 
          <Route path='login' element={<Login/>}/>
+        </Route>
          {/** protected routes - authentication */}
-         <Route path='dash' element={<EmpDashPage/>}>
-          <Route index element={<Welcome/>} />
-          <Route path ='tasks'>
-            <Route index element = {<TasksList />} />
-          </Route>
+
+        <Route path='/dash' element={<EmpLayout/>}>
+            <Route index element={<Welcome/>} />
+            <Route path ='tasks'>
+            <Route index element = {<TasksPage />} />
+            </Route>
+            <Route path= 'notes' element={<NotesPage/>}/>
 
           {/** require authorizaiton */}
           <Route path ='users'>
             <Route index element = {<UsersList />} />
           </Route>
 
-         </Route> {/*end of dashboard routes*/}
+        </Route> {/*end of dashboard routes*/}
          
          <Route path='/Register' element={<Register/>}/>
-        </Route>
+        
       </Routes>
     </div>
     </>
